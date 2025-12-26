a0bcb457bf64b9b780e5455a3297f643134f2fa4
Jacob <jacob@Jacobs-MBP.lan>
2025-12-25 21:40:59 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+
+// Made By Jacob Chamney //
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+} 
+
+app.whenReady().then(() => {
+  createWindow()
+})
+
