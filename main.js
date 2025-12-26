bf8b99246d24ccc3a8eaa79c95ce340f879453bf
Jacob <jacob@Jacobs-MBP.lan>
2025-12-25 22:03:11 -0600

    First Functional Build

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 
 // Made By Jacob Chamney //
 
+const { app, BrowserWindow } = require("electron")
+
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
@@ -12,5 +14,18 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
 })
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  } 
+})
+
 
