45bffcae28eef32b2d89f2ad5e029f20cb45e0a2
Jacob <jacob@Jacobs-MBP.lan>
2025-12-27 22:30:50 -0600

    added viewing

--- main.js
+++ main.js
@@ -6,27 +6,81 @@ const { app, BrowserWindow, ipcMain } = require("electron")
 const Database = require('better-sqlite3');
 const db = new Database('Orders.db', { verbose: console.log });
 
-//Create table 
+//Create Table 
 const createTableSQL = `
   CREATE TABLE IF NOT EXISTS Orders (
   OrderID INTEGER PRIMARY KEY AUTOINCREMENT,
   FirstName TEXT NOT NULL,
   LastName TEXT NOT NULL,
-  OrderDescription TEXT NOT NULL,
+  OrderDesc TEXT NOT NULL,
   OrderPrice REAL NOT NULL,
   PaymentMethod TEXT NOT NULL,
   OrderDate TEXT NOT NULL
 )
   `;
 db.exec(createTableSQL);
+//End Create Table
 
-// IPC Handler
+// Insert Handler
 ipcMain.on('insert-order', (event, orderData) => {
-  
-  
+  const insert = db.prepare(`
+    INSERT INTO Orders (
+      FirstName,
+      LastName,
+      OrderDesc,
+      OrderPrice,
+      PaymentMethod,
+      OrderDate
+    )
+    VALUES (?,?,?,?,?,?)
+  `)
+  insert.run(
+    orderData.FirstName,
+    orderData.LastName,
+    orderData.OrderDesc,
+    orderData.OrderPrice,
+    orderData.PaymentMethod,
+    orderData.OrderDate
+  )
+  const orders = db.prepare(`
+  SELECT *
+  FROM Orders
+  ORDER BY OrderDate DESC
+`).all();
+  event.sender.send('load-orders', orders);
   console.log('Received order data:', orderData);
 });
-//End IPC Handler
+//End Insert Handler
+
+//Update Handler
+ipcMain.on('update-order', (event, orderData) => {
+  const update = db.prepare(`
+  UPDATE Orders 
+  SET FirstName = ?,
+      LastName = ?,
+      OrderDesc = ?,
+      OrderPrice = ?,
+      PaymentMethod = ?,
+      OrderDate = ?
+  WHERE OrderID = ?
+`)
+update.run(
+    orderData.FirstName,
+    orderData.LastName,
+    orderData.OrderDesc,
+    orderData.OrderPrice,
+    orderData.PaymentMethod,
+    orderData.OrderDate,
+    orderData.OrderID
+  )
+  const orders = db.prepare(`
+  SELECT *
+  FROM Orders
+  ORDER BY OrderDate DESC
+`).all();
+  event.sender.send('load-orders', orders);
+  console.log('Updated order data:', orderData);
+});
 
 //Create Window
 const createWindow = () => {
@@ -40,6 +94,19 @@ const createWindow = () => {
   })
 
   win.loadFile('index.html')
+  
+  //Send DB to WebPage
+  win.webContents.on("did-finish-load", () => {
+    const orders = db.prepare(`
+      SELECT *
+      FROM Orders
+      ORDER BY OrderDate DESC
+    `).all();
+
+    win.webContents.send('load-orders', orders);
+  });
+  //End Send Data to WebPage
+
 } 
 
 app.whenReady().then(() => {
--- renderer.js
+++ renderer.js
@@ -1,13 +1,91 @@
+
+const { ipcRenderer } = require('electron')
+
+let selectedOrder = null;
+let selectedRow = null;
+let isEditing = false;
+
+//Submit OrderData To DB
 document.querySelector('form').addEventListener('submit', (e) => {
   e.preventDefault();
 
-  const formData = {
-    firstName: document.getElementById('fname').value,
-    lastName: document.getElementById('lname').value,
-    orderDesc: document.getElementById('orderdesc').value,
-    orderPrice: document.getElementById('orderprice').value,
-    paymentMethod: document.getElementById('paymentmethod').value,
-    date: document.getElementById('date').value
+const formData = {
+  FirstName: document.getElementById('fname').value,
+  LastName: document.getElementById('lname').value,
+  OrderDesc: document.getElementById('orderdesc').value,
+  OrderPrice: document.getElementById('orderprice').value,
+  PaymentMethod: document.getElementById('paymentmethod').value,
+  OrderDate: document.getElementById('date').value
+}
+if(isEditing) {
+    formData.OrderID = selectedOrder.OrderID;
+    ipcRenderer.send('update-order', formData);
+  } else {
+    ipcRenderer.send('insert-order', formData);
   }
-  console.log(formData);
+console.log(formData);
+});
+//End Submit OrderData To DB
+
+//Recieve OrderData
+ipcRenderer.on('load-orders', (event, orders) => {
+  const tbody = document.getElementById('orders-body');
+  tbody.innerHTML = '';
+  orders.forEach(order => {
+   const row = document.createElement('tr');
+    const dateCell = document.createElement('td');
+    dateCell.textContent = order.OrderDate;
+    row.appendChild(dateCell);
+    
+    const lastNameCell = document.createElement('td');
+    lastNameCell.textContent = order.LastName;
+    row.appendChild(lastNameCell);
+    
+    const firstnameCell = document.createElement('td');
+    firstnameCell.textContent = order.FirstName;
+    row.appendChild(firstnameCell);
+    
+    const orderCell = document.createElement('td');
+    orderCell.textContent = order.OrderDesc;
+    row.appendChild(orderCell);
+    
+    const totalCell = document.createElement('td');
+    totalCell.textContent = order.OrderPrice;
+    row.appendChild(totalCell);
+    
+    const paymenttypeCell = document.createElement('td');
+    paymenttypeCell.textContent = order.PaymentMethod;
+    row.appendChild(paymenttypeCell);
+
+    row.addEventListener('click', () => {
+      if(selectedRow) {
+        selectedRow.classList.remove('selected');
+      }
+      row.classList.add('selected');
+      selectedRow = row;
+      selectedOrder = order;
+      document.getElementById('edit-btn').style.display = 'inline-block';
+      document.getElementById('delete-btn').style.display = 'inline-block';
+    });
+
+    tbody.appendChild(row);
   });
+  console.log('Recieved Orders:', orders);
+});
+//End Recieve OrderData
+
+//Edit Order
+document.getElementById('edit-btn').addEventListener('click', () => {
+  document.getElementById('fname').value = selectedOrder.FirstName;
+  document.getElementById('lname').value = selectedOrder.LastName;
+  document.getElementById('orderdesc').value = selectedOrder.OrderDesc;
+  document.getElementById('orderprice').value = selectedOrder.OrderPrice;
+  document.getElementById('paymentmethod').value = selectedOrder.PaymentMethod;
+  document.getElementById('date').value = selectedOrder.OrderDate;
+
+  document.getElementById('submit-btn').style.display = 'none';
+  document.getElementById('delete-btn').style.display = 'none';
+  document.getElementById('edit-btn').textContent = 'Update';
+  isEditing = true;
+});
+//End Edit Order
